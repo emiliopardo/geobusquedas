d3d81033d385164c54a2b7adf4eb4cbad66a0e39
Emilio Pardo <e.pardo.perez@gmail.com>
2023-06-23 13:37:07 +0200

    se añaden controles y capa base

--- test/test.js
+++ test/test.js
@@ -2,11 +2,35 @@ import Geobusquedas from 'facade/geobusquedas';
 
 M.addProxyException("http://localhost");
 
+// const ortofoto2016_color = new M.layer.WMS({
+//   url: 'http://www.ideandalucia.es/wms/ortofoto2016?',
+//   name: 'ortofotografia_2016_rgb',
+//   legend: 'Ortofotografía Color 0,5 metros/pixel (Año 2016)',
+//   transparent: false,
+//   tiled: true
+// }, {
+//   styles: 'default'
+// })
+
+const ortofoto2016_pancromatica = new M.layer.WMS({
+  url: 'http://www.ideandalucia.es/wms/ortofoto2016?',
+  name: 'ortofotografia_2016_pancromatico',
+  legend: 'Ortofotografía Pancromática 0,5 metros/pixel (Año 2016)',
+  transparent: false,
+  tiled: true
+}, {
+  styles: 'default'
+})
+
+
 const map = M.map({
   container: 'mapjs',
+  //layers: [ortofoto2016_color],
+  layers: [ortofoto2016_pancromatica],
+  // projection: "EPSG:4326*d",
 });
 
-map.addControls(['scale', 'layerswitcher']);
+map.addControls(['scale','scaleline','mouse','panzoombar', 'layerswitcher']);
 
 
 var configuracion = {
